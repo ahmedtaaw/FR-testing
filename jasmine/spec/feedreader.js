88517bf8be94c3738ea0c6e111d2249720fc656d
ahmedtaaw <ahmedtaaw@gmail.com>
2018-02-24 18:15:17 +0200

    update

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -77,7 +77,11 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-          
+         it('Menu changes visibility when the menu icon is clicked', function(){
+            $('.menu-icon-link').click();//to check if visiable
+            expect(document.body.classList.contains('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();//to return to initial state
+            });
             
         });
     /* TODO: Write a new test suite named "Initial Entries" */
@@ -88,6 +92,17 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+       beforeEach( function(done){
+        loadFeed(0,function(){
+        done();
+    });
+       });
+       it('should be at least one feed',function(){
+        var feeds = $('.feed a');
+        //console.log("feeds.length",feeds.length);
+        expect(feeds.length).toBeGreaterThan(0);
+      
+          });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
